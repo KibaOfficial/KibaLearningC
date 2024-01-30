7babb82edd7d0dfc100366a66cec6c3421ebcffa
KibaOfficial <kiba@kibaofficial.net>
2024-01-30 01:09:41 +0100

    "Finished Chapter 3"

--- chapter3/_Exercise/ExerciseChapter3.c
+++ chapter3/_Exercise/ExerciseChapter3.c
@@ -2,6 +2,21 @@
 
 int main()
 {
+    int number;
+    int remainder;
+    printf("Please enter a valid unsigned integer!\n");
+    scanf("%d", &number);
+    remainder = number % 3;
+
+    if (remainder != 0)
+    {
+        printf("The number you entered is not div. by 3!\n");
+        printf("The remainder of the operation is: %d\n", remainder);
+    }
+    else
+    {
+        printf("The number you entred is div. by 3\n");
+    }
 
     return 0;
 }
